79197abac2b8b33419be2536231499411e6d64f6
JackieGu-Book <jackieggu@gmail.com>
2021-08-04 22:16:51 +0800

    修改提供者服务使用nacos配置中心

--- provider/src/main/java/cn/jackiegu/spring/cloud/alibaba/provider/service/impl/UserServiceImpl.java
+++ provider/src/main/java/cn/jackiegu/spring/cloud/alibaba/provider/service/impl/UserServiceImpl.java
@@ -4,16 +4,24 @@ import cn.jackiegu.spring.cloud.alibaba.provider.dao.UserDao;
 import cn.jackiegu.spring.cloud.alibaba.provider.model.UserEntity;
 import cn.jackiegu.spring.cloud.alibaba.provider.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope
 public class UserServiceImpl implements UserService {
+
+    @Value("${user.sex}")
+    private String userSex;
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public UserEntity get(Long id) {
-        return userDao.findById(id);
+        UserEntity user = userDao.findById(id);
+        user.setName(user.getName() + ":" + userSex);
+        return user;
     }
 }
